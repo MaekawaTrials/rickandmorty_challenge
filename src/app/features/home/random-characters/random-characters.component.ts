6c112a8ad95f0e210edd6f636c24bf5fd94213f6
Your Name <mrmaekawa@gmail.com>
2024-06-10 21:26:54 -0300

    fix: update card components to correctly check favorite status

--- src/app/features/home/random-characters/random-characters.component.ts
+++ src/app/features/home/random-characters/random-characters.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Character } from '../../../state/character.model';
 import { Observable, of } from 'rxjs';
 import { map, switchMap, catchError } from 'rxjs/operators';
+import { Store, select } from '@ngrx/store';
+import { AppState, selectAllFavorites } from 'src/app/state';
 
 @Component({
   selector: 'app-random-characters',
@@ -12,9 +14,12 @@ import { map, switchMap, catchError } from 'rxjs/operators';
 
 export class RandomCharactersComponent implements OnInit {
   characters$: Observable<Character[]> = of([]);
+  favorites$: Observable<Character[]>;
   @Input() numberOfCharacters: number = 3;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private store: Store<AppState>) {
+    this.favorites$ = this.store.pipe(select(selectAllFavorites));
+  }
 
   ngOnInit(): void {
     this.characters$ = this.fetchAllCharacters().pipe(
@@ -33,5 +38,9 @@ export class RandomCharactersComponent implements OnInit {
   getRandomCharacters(characters: Character[], count: number): Character[] {
     const shuffled = characters.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
+  }
+
+  isFavorite(character: Character, favorites: Character[]): boolean {
+    return favorites.some(fav => fav.id === character.id);
   }
 }
